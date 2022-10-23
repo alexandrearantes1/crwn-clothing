acf9ca7dffc16555be06db035556d745a6362db6
Alexandre Arantes <chan.arantes@gmail.com>
2022-10-23 09:38:31 +0100

    add shopping cart icon and shopping list menu

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,12 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
+
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
@@ -9,6 +14,7 @@ import './navigation.styles.scss';
 
 const Navigation = () => {
    const { currentUser } = useContext(UserContext);
+   const { isCartOpen } = useContext(CartContext);
 
    if (currentUser)
       setTimeout(() => { console.log(currentUser.displayName) }, 500);
@@ -32,7 +38,9 @@ const Navigation = () => {
                         </Link>
                      )
                }
+               <CartIcon />
             </div>
+            {isCartOpen && <CartDropdown />}
          </div>
          <Outlet />
       </Fragment>
